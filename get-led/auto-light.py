71ab20bc1238b54f6fdd9f5c2a5929d44c45ffd4
Knshv1 <konstantinshvet@gmail.com>
2026-09-15 11:39:58 +0300

    Rap4

--- get-led/auto-light.py
+++ get-led/auto-light.py
@@ -4,13 +4,13 @@ import time
 GPIO.setmode(GPIO.BCM)
 led = 26
 GPIO.setup(led, GPIO.OUT)
-lig = 6
-GPIO.setup(lig, GPIO.IN)
+pwm = GPIO.PWM(led, 200)
+duty = 0.0
+pwm.start(duty)
 state = 0
 while True:
-    if GPIO.input(lig):
-        state = 0
-    else:
-        state = 1
-    GPIO.output(led, state)
-    time.sleep(0.2)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.05)
+    duty = duty + 1.0
+    if duty > 100.0:
+        duty = 0.0
